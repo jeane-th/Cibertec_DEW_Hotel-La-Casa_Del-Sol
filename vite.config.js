3ec75557955ffd2f7547477e0e782727b6d0e2b2
Jeaneth Tafur <86864116+jeane-th@users.noreply.github.com>
2025-08-23 22:28:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Cibertec_DEW_Hotel-La-Casa_Del-Sol/', // <- agrega esta línea
 })
