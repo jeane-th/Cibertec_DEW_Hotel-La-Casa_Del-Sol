f30ce0dc06b2d48caa4a9f9f8a6ede3f8b7e939a
DesXp <vasquezdesiderio@gmail.com>
2025-08-23 20:11:02 -0500

    Actualización de footer y logos inferior y superior

--- src/components/BarraNavegacion.jsx
+++ src/components/BarraNavegacion.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import logo from '../assets/images/Logo.png'
+import logo from '../assets/images/Logo-header.jpeg'
 import { Link } from 'react-router-dom';
 
 export const BarraNavegacion = () => {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import LogoWhatsApp from '../assets/images/RedesSociales/WhatsApp.png'
+import LogoWhatsApp from '../assets/images/RedesSociales/Whatsapp.png'
 import LogoInstagram from '../assets/images/RedesSociales/Instagram.png'
 import LogoFacebook from '../assets/images/RedesSociales/Facebook.png'
+import logo from '../assets/images/RedesSociales/soloLogo.png'
+import logoDos from '../assets/images/RedesSociales/Logo2.png'
+
+import '../css/footer.css'
 
 const Redes = [
   { alt: "WhatsApp", imagen: LogoWhatsApp, url: "https://web.whatsapp.com"},
@@ -16,12 +20,22 @@ export const Footer = () => {
     <footer id="contact" className="footer">
       <div className="container">
         <div className="footer-contenido">
-          <div>
+          <div className='footer-info'>
             <h4>Información Adicional</h4>
                 <p><strong>Dirección:</strong> Av. Carlos Izaguirre 233, Independencia 15311</p>
                 <p><strong>Teléfono</strong> +51 916325859</p>
                 <p><strong>Teléfono / WhatsApp:</strong> <a href="https://wa.me/51916325859" target="_blank">+51 916325859</a></p>
-            <p><strong>Email:</strong> <a href="mailto:contacto@lacasadelsol.com">contacto@lacasadelsol.com</a></p>
+            <p><strong>Email:</strong> <a href="mailto:contacto@hotellacasadelsol.com">contacto@hotellacasadelsol.com</a></p>
+          </div>
+          <div className='logo-footer'>
+            
+
+            <img  src={logo} alt="" />
+            <img src={logoDos} alt="" />
+
+            
+            
+
           </div>
           <div className="footer-redes">
             <ul>
@@ -37,7 +51,7 @@ export const Footer = () => {
           </div>
         </div>
         <div className="footer-fondo">
-          <p>&copy; 2025 La Casa del Sol</p>
+          <p>&copy; 2025 Hotel La Casa del Sol</p>
         </div>
       </div>
     </footer>
