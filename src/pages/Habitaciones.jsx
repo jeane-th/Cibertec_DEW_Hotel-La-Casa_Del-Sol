979c157037f56f8365cfe279b5caa330ac44c208
Jeaneth Tafur <jtafurh1204@gmail.com>
2025-08-24 13:38:40 -0500

    Updates

--- src/pages/Habitaciones.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Habitaciones = () => {
-  return (
-    <div>PaginaHabitaciones</div>
-  )
-}
-
-export default Habitaciones
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Cibertec_DEW_Hotel-La-Casa_Del-Sol/', // <- agrega esta línea
-})
